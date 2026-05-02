9f64f79d990446de26d331acb836230f5fec77db
Lacci <lannadosanjoscosta@gmail.com>
2026-05-02 09:31:32 -0300

    feat: add queries header file

---
+++ src/queries.h
@@ -0,0 +1,10 @@
+#ifndef QUERIES_H
+#define QUERIES_H
+
+#include "avl.h"
+
+int query_ativos_por_magnitude(AVL *avl, int mag_min, int mag_max);
+int query_ativos_por_regiao(AVL *avl, const char *regiao);
+int query_eventos_por_intervalo(AVL *avl. int id_min, int id_max);
+
+#endif
