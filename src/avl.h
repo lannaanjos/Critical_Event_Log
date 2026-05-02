243177f4f1f4b5342297145fe6b8bca464b6c846
Lacci <lannadosanjoscosta@gmail.com>
2026-05-01 23:11:46 -0300

    feat: instance functions and add structs on avl header

---
+++ src/avl.h
@@ -0,0 +1,44 @@
+#ifndef AVL_H
+#define AVL_H
+
+#include "event.h"
+
+typedef struct NoAVL{
+  Evento evento;
+  int altura;
+  struct NoAVL *esquerda;
+  struct NoAVL *direita;
+} NoAVL;
+
+typedef struct {
+  NoAVL *raiz;
+  int total_nos;
+  int total_ativos;
+  int total_rotacoes;
+} AVL;
+
+// funcs de inicialização
+AVL *criar_avl(void);
+void destruir_avl(AVL *avl);
+
+// funcs auxiliares
+int get_altura_no(NoAVL *no);
+int get_fator_balanceamento(NoAVL *no);
+void atualiza_altura(NoAVL *no);
+NoAVL *rotacao_direita(AVL *avl, NoAVL *y);
+NoAVL *rotacao_esquerda(AVL *avl, NoAVL *x);
+NoAVL *rot_dupla_esq_dir(AVL *avl, NoAVL *no);
+NoAVL *rot_dupla_dir_esq(AVL *avl, NoAVL *no);
+NoAVL *rebalancear(AVL *avl, NoAVL *no);
+
+NoAVL *_no_minimo(NoAVL *no);
+
+// /\/\/\ operações principais
+int avl_inserir_evento(AVL *avl, Evento evento);
+int avl_remover_evento(AVL *avl, int id);
+NoAVL *avl_buscar_evento(AVL *avl, int id);
+
+// ops de atualização
+int avl_atualizar_status(AVL *avl, int id, StatusEvento novo_status);
+int avl_atualizar_magnitude(AVL *avl, int id, int nova_magnitude);
+
